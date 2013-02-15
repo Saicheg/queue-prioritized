5a114af78a7f18f9aa82ea58162cbb4f60cf9545
Valentine Zavadsky <saicheg@gmail.com>
2013-02-15 21:21:14 +0300

    implement Task class

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,4 +2,41 @@ require_relative 'spec_helper'
 
 describe Task do
 
+  describe 'iniitialization' do
+
+    it 'should create valid task object' do
+      expect { Task.new(finish_time: Time.now, description: 'foo') }.to_not raise_error
+    end
+
+    it 'should filter invalid param sex on initialize' do
+      expect { Task.new(finish_time: Time.now, description: 'foo', sex: 'bar') }.to raise_error
+    end
+
+    it 'should be fine if only 1 argument is passed' do
+      expect { Task.new(description: 'foo') }.to_not raise_error
+    end
+
+  end
+
+  describe 'behaviour' do
+    context 'user is trying to pass invalid type arguments' do
+      subject { Task.new }
+
+      it 'should not allow string format for finish_time param' do
+        expect { subject.finish_time = 'foobar' }.to raise_error
+      end
+
+      it 'should allow time format for finish_time param' do
+        expect { subject.finish_time = Time.now }.to_not raise_error
+      end
+
+      it 'should not allow other format either string for descrption param' do
+        expect { subject.description = 42 }.to raise_error
+      end
+
+      it 'should allow string format for description param' do
+        expect { subject.description = 'foobar' }.to_not raise_error
+      end
+    end
+  end
 end
--- task.rb
+++ task.rb
@@ -1,4 +1,28 @@
 class Task
-  #TODO
-end
 
+  ATTRIBUTES = :finish_time, :description
+
+  attr_reader *ATTRIBUTES
+
+  def initialize(params={})
+    raise ArgumentError unless valid_attributes?(params.keys)
+    params.each { |value, key| send("#{value}=", key) }
+  end
+
+  def finish_time=(attr)
+    raise ArgumentError unless attr.is_a?(Time)
+    @finish_time = attr
+  end
+
+  def description=(attr)
+    raise ArgumentError unless attr.is_a?(String)
+    @description = attr
+  end
+
+  private
+
+  def valid_attributes?(attrs)
+    (ATTRIBUTES | attrs) == ATTRIBUTES
+  end
+
+end
