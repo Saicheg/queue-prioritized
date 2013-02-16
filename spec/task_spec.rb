f3231c5b5d86c982d8b641fb63bad66ee0be7fa6
Valentine Zavadsky <saicheg@gmail.com>
2013-02-16 15:05:57 +0300

    use factories where it is possible

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -5,15 +5,19 @@ describe Task do
   describe 'iniitialization' do
 
     it 'should create valid task object' do
-      expect { Task.new(finish_time: Time.now, description: 'foo') }.to_not raise_error
+      expect { FactoryGirl.build(:task) }.to_not raise_error(ArgumentError)
     end
 
     it 'should filter invalid param sex on initialize' do
-      expect { Task.new(finish_time: Time.now, description: 'foo', sex: 'bar') }.to raise_error
+      expect { Task.new(sex: 'bar') }.to raise_error(ArgumentError)
     end
 
     it 'should be fine if only 1 argument is passed' do
-      expect { Task.new(description: 'foo') }.to_not raise_error
+      expect { Task.new(description: 'foo') }.to_not raise_error(ArgumentError)
+    end
+
+    it 'should be fine if no arguments is passed' do
+      expect { Task.new }.to_not raise_error(ArgumentError)
     end
 
   end
@@ -23,25 +27,25 @@ describe Task do
       subject { Task.new }
 
       it 'should not allow string format for finish_time param' do
-        expect { subject.finish_time = 'foobar' }.to raise_error
+        expect { subject.finish_time = 'foobar' }.to raise_error(ArgumentError)
       end
 
       it 'should allow time format for finish_time param' do
-        expect { subject.finish_time = Time.now }.to_not raise_error
+        expect { subject.finish_time = Time.now }.to_not raise_error(ArgumentError)
       end
 
       it 'should not allow other format either string for descrption param' do
-        expect { subject.description = 42 }.to raise_error
+        expect { subject.description = 42 }.to raise_error(ArgumentError)
       end
 
       it 'should allow string format for description param' do
-        expect { subject.description = 'foobar' }.to_not raise_error
+        expect { subject.description = 'foobar' }.to_not raise_error(ArgumentError)
       end
     end
   end
 
   describe 'serialization/deserialization' do
-    subject { Task.new(finish_time: Time.now, description: 'foo') }
+    subject { FactoryGirl.build(:task) }
 
     it 'should be serialized to json' do
       subject.to_json.should == {finish_time: subject.finish_time, description: subject.description}.to_json
